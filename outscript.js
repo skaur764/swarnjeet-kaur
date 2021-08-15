eb1339d66fea1d99ad99f620f40725cbe0771b29
skaur764 <88954970+skaur764@users.noreply.github.com>
2021-08-14 22:38:19 -0400

    Add files via upload

---
+++ outscript.js
@@ -0,0 +1,8 @@
+function showhourratefield()
+{
+document.getElementById("hourrate").style.display="block";	
+}
+function hidehourratefield()
+{
+document.getElementById("hourrate").style.display="none";	
+}
